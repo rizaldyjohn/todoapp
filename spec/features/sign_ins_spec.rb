2ed1a73c0a25786829ea878d7a008663da3f63af
rizaldyjohn <rj.bansagan@twitmusic.com>
2015-03-31 17:59:22 +0800

    modified sign in form for task spec

--- spec/features/sign_ins_spec.rb
+++ spec/features/sign_ins_spec.rb
@@ -23,10 +23,10 @@ RSpec.describe 'Signing in', type: :feature do
       it 'should return an error if email or password is invalid' do
         visit new_session_path
 
-        within('form') do
+       # within('form') do
           fill_in 'email', with: 'a'
           fill_in 'password', with: 'b'
-        end
+       # end
 
         click_button 'Sign in'
         expect(page).to have_content('Invalid email or password')
@@ -43,13 +43,13 @@ RSpec.describe 'Signing in', type: :feature do
         sarah_smith = FactoryGirl.create(:user, first_name: 'Sarah', last_name: 'Smith', email: 'abc1@yopmail.com', password: 'testing')
         visit new_sign_up_path
 
-        within('form') do
+       # within('form') do
           fill_in 'user_first_name', with: sarah_smith.first_name
           fill_in 'user_last_name', with: sarah_smith.last_name
           fill_in 'user_email', with: 'abc@yopmail.com'
           fill_in 'user_password', with: sarah_smith.password
           fill_in 'user_password_confirmation', with: sarah_smith.password
-        end
+       # end
 
         click_button 'Create User'
         expect(page).to have_content('User was successfully created.')
@@ -61,13 +61,13 @@ RSpec.describe 'Signing in', type: :feature do
         sarah_smith = FactoryGirl.create(:user, first_name: 'Sarah', last_name: 'Smith', email: 'abc1@yopmail.com', password: 'testing')
         visit new_sign_up_path
 
-        within('form') do
+       # within('form') do
           fill_in 'user_first_name', with: sarah_smith.first_name
           fill_in 'user_last_name', with: sarah_smith.last_name
           fill_in 'user_email', with: sarah_smith.email
           fill_in 'user_password', with: sarah_smith.password
           fill_in 'user_password_confirmation', with: sarah_smith.password
-        end
+       # end
 
         click_button 'Create User'
         expect(page).to have_content('Email has already been taken')
@@ -78,13 +78,13 @@ RSpec.describe 'Signing in', type: :feature do
         sarah_smith = FactoryGirl.create(:user, first_name: 'Sarah', last_name: 'Smith', email: 'abc1@yopmail.com', password: 'testing')
         visit new_sign_up_path
 
-        within('form') do
+       # within('form') do
           fill_in 'user_first_name', with: sarah_smith.first_name
           fill_in 'user_last_name', with: sarah_smith.last_name
           fill_in 'user_email', with: ' '
           fill_in 'user_password', with: ' '
           fill_in 'user_password_confirmation', with: sarah_smith.password
-        end
+       # end
 
         click_button 'Create User'
         expect(page).to have_content('Email is invalid') 
@@ -96,13 +96,13 @@ RSpec.describe 'Signing in', type: :feature do
         sarah_smith = FactoryGirl.create(:user, first_name: 'Sarah', last_name: 'Smith', email: 'abc1@yopmail.com', password: 'testing')
         visit new_sign_up_path
 
-        within('form') do
+       # within('form') do
           fill_in 'user_first_name', with: sarah_smith.first_name
           fill_in 'user_last_name', with: sarah_smith.last_name
           fill_in 'user_email', with: sarah_smith.email
           fill_in 'user_password', with: 'abcde'
           fill_in 'user_password_confirmation', with: sarah_smith.password
-        end
+       # end
 
         click_button 'Create User'
         expect(page).to have_content("Password confirmation doesn't match Password") 
@@ -112,10 +112,10 @@ RSpec.describe 'Signing in', type: :feature do
         sarah_smith = FactoryGirl.create(:user, first_name: 'Sarah', last_name: 'Smith', email: 'abc1@yopmail.com', password: 'testing')
         visit new_session_path
 
-        within('form') do
+       # within('form') do
           fill_in 'email', with: sarah_smith.email
           fill_in 'password', with: sarah_smith.password
-        end
+       # end
 
         click_button 'Sign in'
         expect(page).to have_content('Signed in successfully')
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,66 +1,123 @@
 require 'rails_helper'
-
+ 
 RSpec.describe 'list of tasks', type: :feature do
-	describe 'GET /dashboard' do
+  describe 'GET /dashboard/tasks' do
+ 
+#    it 'should not allow guest user to visit task dashboard' do
+#      visit dashboard_root_path    
+#    end
+ 
+    before :each do
+      sarah_smith = FactoryGirl.create(:user, email: 'abc1@yopmail.com', password: 'testing')
+      visit root_path
+      click_link 'Sign in'
 
-    pending 'Guests should not access this page'
+        fill_in 'email', with: sarah_smith.email
+        fill_in 'password', with: sarah_smith.password 
 
-		it 'should allow the user visit task' do
-  		visit dashboard_root_path
-  		expect(page).to have_content('Task')
-      visit dashboard_tasks_path
-      expect(page).to have_content('New task')
-  	end
+      click_button 'Sign in'
+      expect(page).to have_content('Signed in successfully')
+    end 
 
-    context 'allows user to create task and validate empty fields' do
+      it 'should allow the user to visit task dashboard' do
+        visit dashboard_root_path
+        expect(page).to have_content('Task')
+      end
+ 
+      it 'should redirect the user to Listing task' do
+        visit dashboard_tasks_path
+        expect(page).to have_content('New task')
+      end
+
       it 'it should validate the user if the fields is empty' do
         visit new_dashboard_task_path
         expect(page).to have_content('New Task')
-
-        within('form') do
+ 
           fill_in 'task_title', with: ' '
           fill_in 'task_description', with: ' '
-        end
-
-        expect(page).to have_content('Yes')
-        expect(page).to have_content('No')
+ 
         click_button 'Create Task'
         expect(page).to have_content("Title can't be blank")
         expect(page).to have_content("Description can't be blank")
       end
-
+    
+    context 'when user create, view and edit a task' do
       it 'should allow user to create new task' do
         visit new_dashboard_task_path
         expect(page).to have_content('New Task')
-
-        within('form') do
-          fill_in 'task_title', with: ' '
-          fill_in 'task_description', with: ' '
-        end
-
-        expect(page).to have_content('Yes')
-        expect(page).to have_content('No')
+ 
+          fill_in 'task_title', with: 'abc1'
+          fill_in 'task_description', with: 'abc2'
+        
         click_button 'Create Task'
       end
-    end
 
-    context 'when user is done creating a task' do
-      it 'sould show the task created' do
+      it 'should show the task created' do
         visit  dashboard_tasks_path
+        
         expect(page).to have_content('Listing tasks')
         expect(page).to have_content('Title')
         expect(page).to have_content('Description')
         expect(page).to have_content('Completed')
       end
 
-      it 'should allow the user to visit edit' do
-       visit edit_dashboard_task_path
-       expect(page).to have_content('Editing task')
-       expect(page).to have_content('Update Task')
-     end
+      it 'should allow the user to edit a task' do
+        visit new_dashboard_task_path
+        expect(page).to have_content('New Task')
+
+          fill_in 'task_title', with: 'abc1'
+          fill_in 'task_description', with: 'abc2'
+
+        click_button 'Create Task'
+
+        click_link 'Edit'
+        fill_in 'task_title', with: 'abc3'
+        fill_in 'task_description', with: 'abc4'
+
+        click_button 'Update Task'
+        expect(page).to have_content('Task was successfully updated.') 
+      end
     end
 
-    pending 'when user will delete a task'
+    context 'when user will destroy the created task' do
+      #still checking why this statement deletes the task without selecting the 'Ok button' on the alert box.   
+      it 'should allow the user to delete a task' do
+        visit new_dashboard_task_path
+        expect(page).to have_content('New Task')
+ 
+          fill_in 'task_title', with: 'abc1'
+          fill_in 'task_description', with: 'abc2'
+       
+        click_button 'Create Task'
+
+        visit dashboard_tasks_path
+     
+        click_link 'Destroy'
+        #alert box doesn't appear after clicking destroy.
+        expect(page).to have_content('Task was successfully destroyed.')
+       
+      end
+    end  
+  end
+end   
+    #context 'when user is done creating a task' do
+    #  it 'should show the task created' do
+    #    visit  dashboard_tasks_path
+    #    expect(page).to have_content('Listing tasks')
+    #    expect(page).to have_content('Title')
+    #    expect(page).to have_content('Description')
+    #    expect(page).to have_content('Completed')
+    #  end
+ 
+    #  it 'should allow the user to visit edit' do
+    #    click_link 'Edit'
+    #    visit  dashboard_tasks_path
+    #    expect(page).to have_content('Update Task')
+    #    expect(page).to have_content('Listing Task')
+    # end
+    #end
+ 
+    #pending 'when user will delete a task'
     #   pending  context 'when user will delete a task' do
     #    it 'should allow the user to delete a task' do
     #     expect { delete :destroy, id: dashboard_task  }
@@ -69,5 +126,5 @@ RSpec.describe 'list of tasks', type: :feature do
     #     expect {}
     #   end
     # end
-  end
-end
+ 
+    #end
